f3c4b23b0219c6f5e7113660cd4777c246143284
Sayi Keshkar <sayi.b.k@gmail.com>
2022-04-24 07:47:29 -0700

    3

--- projectname1/src/testClass.java
+++
@@ -1,8 +0,0 @@
-public class testClass {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-    public String getStr(){
-        return "Test String";
-    }
-}
